0594b206f68f7d96e8b66124b78f3b16f7848f63
antonychristos83 <antonychristos83@gmail.com>
2022-07-09 22:41:10 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestElemArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number  :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("\nEnter element " + (i + 1));
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("\nARRAY ELEMENTS ARE ");
+            Console.WriteLine("-------------------------");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + "\t");
+            }
+            Console.Write("\n");
+            Array.Sort(arr);
+            Console.WriteLine("1st largest element in the array is " + arr[(n - 1)] + " and the 2nd largest element in the array is " + arr[(n - 2)]);
+
+        }
+    }
+}
+
+
+
+
